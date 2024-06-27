5dbef99219357d928dff8a354cc524178210261c
sumanth <sumanth@sulopa.com>
2024-06-27 17:03:34 +0530

    removed customerProfilePic column

--- urban-company-backend/src/vendure-config.ts
+++ urban-company-backend/src/vendure-config.ts
@@ -46,7 +46,7 @@ export const config: VendureConfig = {
         // See the README.md "Migrations" section for an explanation of
         // the `synchronize` and `migrations` options.
         synchronize: false,
-        migrations: [path.join(__dirname, './migrations/*.+(js|ts)')],
+        migrations: [path.join(__dirname, '../migrations/*.+(js|ts)')],
         logging: false,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
