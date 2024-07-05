e918fa3d4d14bd4847e2a94c3940fec987f88582
yash <yashvinderbhandari@gmail.com>
2024-07-05 17:09:43 +0530

    Reapply "In HomeScreen make changes in best salon part"
    This reverts commit c019efe83d086e1f45de6c9fa4e3fcb7ab51e6e3.

--- urban-comany-ui/services/salonDetails.js
+++ urban-comany-ui/services/salonDetails.js
@@ -1,4 +1,58 @@
 
+// import { gql } from "@apollo/client";
+
+// export const GET_PRODUCT_DETAIL = gql`
+// query GetProductDetail($slug: String!) {
+//   product(slug: $slug) {
+//     id
+//     name
+//     description
+//     variants {
+//       id
+//       name
+//       price
+//       priceWithTax
+//       sku
+//       options {
+//         code
+//         name
+//       }
+//     }
+//     featuredAsset {
+//       id
+//       width
+//       height
+//       name
+//       preview
+//       focalPoint {
+//         x
+//         y
+//       }
+//     }
+//     assets {
+//       id
+//       width
+//       height
+//       name
+//       preview
+//       focalPoint {
+//         x
+//         y
+//       }
+//     }
+//     collections {
+//       id
+//       slug
+//       breadcrumbs {
+//         id
+//         name
+//         slug
+//       }
+//     }
+//   }
+// }
+// `;
+
 import { gql } from "@apollo/client";
 
 export const GET_PRODUCT_DETAIL = gql`
@@ -20,10 +74,10 @@ query GetProductDetail($slug: String!) {
     }
     featuredAsset {
       id
+      preview  # Assuming 'preview' is a URL or base64-encoded image data
       width
       height
       name
-      preview
       focalPoint {
         x
         y
@@ -31,10 +85,10 @@ query GetProductDetail($slug: String!) {
     }
     assets {
       id
+      preview  # Assuming 'preview' is a URL or base64-encoded image data
       width
       height
       name
-      preview
       focalPoint {
         x
         y
